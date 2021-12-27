10b9aa9eb3288655c0674930edd9004c36ba2514
Pollie1953 <92685417+Pollie1953@users.noreply.github.com>
2021-12-27 21:56:09 +0100

    End of Section 3

---
+++ Data/-- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO  'USERS' (Id, UserName)
+VALUES (1,"Bob");
+
+INSERT INTO  'USERS' (Id, UserName)
+VALUES (2,"Tom");
+
+INSERT INTO  'USERS' (Id, UserName)
+VALUES (3,"Jane");
